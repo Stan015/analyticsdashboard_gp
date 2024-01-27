415cb3028c9e4c6d5ecbe3d5f6baa2e3f692bb6b
Stanley Azi <azistanley17@gmail.com>
2024-01-27 16:49:57 +0100

    new commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Geegpay Design and Front-End Challenge: Unleash Your Creativity
+
+## Task: Analytics Dashboard
+
+I participated in this wonderful hackathon by [Geegpay](https://twitter.com/geegpay_hq) x [David Herbert](https://twitter.com/DaveyHert) to produce a pixel-perfect code of the given challenge figma file. I am happy to have gained alot of additional knowledge while building this Analytics Dashboard.
+
+Given figma design:
+
+![Given figma design](images/DashboardFigma.png)
+
+## My Goals:
+
+* To produce a pixel-perfect result of the given design
+* Make it responsive to screens
+* Make it accessible to screen readers
+* Able to switch between theme colors
+* Submit before deadline and use relavant hash tags
+
+## Goals Status:
+
+I achieved all the goals listed above and more.
+
+I was supper exiting using relative units where necessary for responsivity with the aid of a tool I built recently; [iConvertUnits](https://iconvertunits.vercel.app/).
+
+## A quick glance at the pixel-perfect code of mine
+
+Desktop view (light theme):
+
+![desktop v light](images/whiteBgCode.png)
+
+Desktop view (Dark theme):
+
+![desktop v dark](/images/darkThemCode.png)
+
+Mobile view (light theme):
+
+![Mobile v light](/images/whiteTmCode.png)
+
+Mobile view (dark theme):
+
+![Mobile view dark](/images/darkTMobile.png)
+
+Mobile view menu open:
+
+![mbile v](/images/DarkTmclose.png)
--- script.js
+++ script.js
@@ -25,6 +25,72 @@ navLinks.forEach(navLink => {
 });
 //
 
+// toggle burger menu 
+const toggleBurgerMenu = () => {
+    const burger = document.querySelector('.burger');
+    const burgerLine = document.querySelectorAll('.burger_line');
+    const navlinks = document.querySelector('header .general_navs_div');
+
+    burger.addEventListener('click', (e) => {
+        e.stopPropagation();
+
+        burgerLine[0].classList.toggle('burger_line1');
+        burgerLine[1].classList.toggle('burger_line2');
+        burgerLine[2].classList.toggle('burger_line3');
+
+        navlinks.classList.toggle('show_header_nav');
+    });
+
+    // Click event on the document
+    document.documentElement.addEventListener('click', () => {
+        // Close the navigation menu if it's open
+        if (navlinks.classList.contains('show_header_nav')) {
+            navlinks.classList.remove('show_header_nav');
+            burgerLine.forEach(line => line.classList.remove('burger_line1', 'burger_line2', 'burger_line3'));
+        }
+    });
+};
+
+toggleBurgerMenu();
+// 
+
+//update todays date 
+const displayCurrentDate = document.querySelector('.todays_date');
+const currentDate = new Date();
+
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+const formattedDate = currentDate.toLocaleDateString('en-US', options);
+
+displayCurrentDate.innerText = formattedDate;
+//
+
+//toggle user profile name to show/hide on specified media query
+const userHeaderProfile = document.querySelector('.title_section .user_div');
+const userNameDetail = document.querySelector('.user_div .user_name');
+
+function toggleUserDetails() {
+    userHeaderProfile.addEventListener('click', (e) => {
+        e.stopPropagation();
+
+        userNameDetail.classList.toggle('show_user_name')
+    })
+};
+
+const mediaQueryWidth = window.matchMedia('(max-width: 940px)');
+toggleUserDetails();
+
+mediaQueryWidth.addEventListener('change', toggleUserDetails);
+
+document.documentElement.addEventListener('click', () => {
+
+    if(userNameDetail.classList.contains('show_user_name')) {
+        userNameDetail.classList.remove('show_user_name');
+    } else {
+        return
+    }
+})
+//
+
 //sales trend bar chart
 const salesTrendChartFunc = () => {
     const salesTrendChart = document.getElementById('bar_chart').getContext('2d');
@@ -106,4 +172,55 @@ const salesTrendChartFunc = () => {
 };
 
 salesTrendChartFunc();
-// //
+// 
+
+//switch theme color
+const themeColorContainer = document.querySelector('.theme_color');
+const themeColorButtons = document.querySelectorAll('.theme_color button');
+const lightTheme = themeColorButtons[0];
+const darkTheme = themeColorButtons[1];
+
+//grab bg color and color 
+const header = document.querySelector('header');
+const headerDiv = document.querySelector('header .general_navs_div');
+const headerContainer = document.querySelector('header .container');
+const whiteBgElements = document.querySelectorAll('.white_bg');
+
+themeColorButtons.forEach(themeColorButton => {
+    themeColorButton.addEventListener('click', (e) => {
+        const currentTheme = e.target;
+
+
+        console.log(currentTheme)
+        if(currentTheme === lightTheme) {
+            darkTheme.setAttribute('aria-current', 'false');
+            lightTheme.setAttribute('aria-current', 'true');
+
+            document.body.style.backgroundColor = '#FAFAFA';
+            document.documentElement.style.setProperty('--clr-primary', '#26282C')
+            header.style.backgroundColor = '#FFFFFF';
+            headerDiv.style.backgroundColor = '#F7F8FA';
+            headerContainer.style.backgroundColor = '#F7F8FA';
+            whiteBgElements.forEach(whiteBgElement => {
+                whiteBgElement.style.backgroundColor = '#FFFFFF';
+            });
+            themeColorContainer.style.backgroundColor = '#FFFFFF';
+        } 
+        
+        if(currentTheme === darkTheme) {
+            lightTheme.setAttribute('aria-current', 'false');
+            darkTheme.setAttribute('aria-current', 'true');
+            
+            document.body.style.backgroundColor = '#092544';
+            document.documentElement.style.setProperty('--clr-primary', '#FFFFFF');
+            header.style.backgroundColor = '#092544';
+            headerDiv.style.backgroundColor = '#092544';
+            headerContainer.style.backgroundColor = '#092544';
+            whiteBgElements.forEach(whiteBgElement => {
+                whiteBgElement.style.backgroundColor = '#092544'
+            });
+            themeColorContainer.style.backgroundColor = '#092544';
+        } 
+    })
+});
+//
